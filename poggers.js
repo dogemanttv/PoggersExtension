0807635ac8ca30be507701b17ed27fbff6de70fc
dogemanwastaken <80775876+dogemanwastaken@users.noreply.github.com>
2021-12-07 18:02:07 -0600

    Add files via upload

---
+++ poggers.js
@@ -0,0 +1,7 @@
+function replacepog() {
+for (let num = 0; num < 99999999; num++) {
+    document.images[num].src="https://i.imgur.com/KEIsKUA.png";
+    num + 1;
+  }
+}
+window.setInterval(replacepog, 200);
